c560266ef9806c64a853c330911ed95f8b6ae0b3
Surya Mallidi <surya_mallidi@outlook.com>
2025-01-30 13:33:42 -0600

    Completed the base functionality of the hangman game, not optimized

--- hangman.c
+++ hangman.c
@@ -4,13 +4,39 @@
 #include <time.h> //for the seed function to actually make it give random numbers, seed determines the sequence of psudo random numebrs so if no srand then same sequence all the time
 
 char* selectedWord();
+void checkLetter(char aLetter, char* chosenWord, char* current, int size, int* remaining);
 
 int main(){
     srand(time(NULL)); //time function with NULL returns time as seconds since january 1, 1970, so seed of rand set to a new second each time
-    char* chosenWord = selectedWord();
-    for(int i = 0; i < 5; i++){
-        printf("%c", chosenWord[i]);
+    char* chosenWord = selectedWord(); //The word chosen that the user has to guess, has a default size of 8 bytes
+    int lives = 6; //The number of lives the user has to guess the word
+    char usersLetter; //The array that houses 
+    int size = strlen(chosenWord); //Size of the chosen word array
+    int lettersRemaining = size; //Keeps track of the number of letters needed for the user to guess
+
+    char correctLetters[size]; //Shows the user the letters that have been discovered so far
+    for(int i = 0; i < size; i++){
+        correctLetters[i] = '_';
     }
+
+    printf("Welcome to Hangman!\n");
+    while(lettersRemaining != 0 && lives > 0){
+        for(int i = 0; i < size; i++){
+            printf("%c", correctLetters[i]);
+        }
+
+        printf("\nEnter your chosen letter: ");
+        scanf("%c", &usersLetter);
+        getchar();
+
+        checkLetter(usersLetter, chosenWord, correctLetters, size, &lettersRemaining);
+    }
+
+    printf("The word was: ");
+    for(int i = 0; i < size; i++){
+        printf("%c", correctLetters[i]);
+    }
+    
     return 0;
 }
 
@@ -27,5 +53,17 @@ char* selectedWord(){ //Learn arrays first, then strings, memory access/pointers
             break;
         default:
             break;
+    }
+}
+
+/*
+*Checks if the user's letter that was inputed is part of the chosen word adding that letter to the display and reducing the number of letters remaining by the number of matches
+*/
+void checkLetter(char aLetter, char* chosenWord, char* current, int size, int* remaining){
+    for(int i = 0; i < size; i++){
+        if(aLetter == *(chosenWord + i)){
+            *(current + i) = aLetter;
+            (*remaining)--; //reduces the number of letters that the user needs to guess
+        }
     }
 }
